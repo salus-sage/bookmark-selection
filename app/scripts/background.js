f552d621ab1487c73bab8acdb82417f271d70ff8
salus-sage <salus.sage@gmail.com>
2019-09-26 21:27:50 +0530

    First commit integrated with quoteURL

---
+++ app/scripts/background.js
@@ -0,0 +1,45 @@
+browser.runtime.onInstalled.addListener((details) => {
+  console.log('previousVersion', details.previousVersion)
+});
+
+browser.browserAction.setBadgeText({
+  text: `'Allo'`
+});
+
+console.log(`'Allo 'Allo! Event Page for Browser Action`)
+
+var onCreated = function(e){
+	if (browser.runtime.lastError) {
+	    console.log("error creating item:" + browser.runtime.lastError);
+	  } else {
+	    console.log("item created successfully");
+
+	  }
+}
+
+//Create Bookmark
+var createBookmark = function(info, tab){
+	var createBookmark = browser.bookmarks.create({
+	  title: tab.title,
+	  url: tab.url
+	});
+	return createBookmark;
+}
+//Create context menu
+browser.contextMenus.create({
+  id: "log-selection",
+  title: "Bookmark Selection '%s'",
+  contexts: ["selection"]
+}, onCreated);
+
+browser.contextMenus.onClicked.addListener(function(info, tab) {
+	console.log(info, tab, browser);
+  switch (info.menuItemId) {
+    case "log-selection":
+      console.log(info.selectionText);
+      var created = createBookmark(info, tab);
+      console.log(created, "Bookmark Created")
+      break;
+    
+  }
+})
---
+++ app/scripts/contentscript.js
@@ -0,0 +1,4 @@
+console.log(`'Allo 'Allo! Content script`)
+
+// Import the quote URL script
+import './lib/quoteurl.min.js'
